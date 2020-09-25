b3e27aa0202e47e89d0bb641cf7d9424bcd66b0c
wormi <andreas.worm@headissue.com>
2020-09-25 13:31:41 +0200

    test local storage

--- src/ExploreData.tsx
+++ src/ExploreData.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import TestOutcome from "./model/TestOutcome";
 import Outcome from "./Outcome";
-import {Box} from "@material-ui/core";
+import {Box, Button, Paper} from "@material-ui/core";
 import {useLocation, useParams} from "react-router";
 import qs from "query-string"
+import useLocalStorage from "react-use-localstorage";
+import {Link} from "react-router-dom";
 //import Index from "../public/outcomes/full-json/index.json"
 
 declare global {
@@ -12,15 +14,25 @@ declare global {
   }
 }
 
-
 const ExploreData = () => {
 
   const [outcomes, setOutcomes] = useState<Array<TestOutcome>>(window.outcomes);
 
   const location = useLocation();
   const query = qs.parse(location.search)
+  const [detail, setDetail] = useLocalStorage('detail', "1");
+
 
   return <>
+    <Paper style={{padding: "1rem", margin: "1rem"}}>
+      <p><Button variant="contained" color="primary" disableElevation href="#anchor">test anchorlink</Button><br/>
+        <Button variant="contained" color="primary" disableElevation href="?id=4&id=5&detail=high">test param link</Button><br/>
+        <Link to="#anchor">router anchor test</Link> (does not jump to anchor)<br/>
+        <Link to="?id=4&id=5&detail=high">router query test</Link> (does update location and causes rerender 👍)<br/>
+      </p>
+      <p>to test local storage: <Button variant="contained" color="primary" disableElevation onClick={() => setDetail((parseInt(detail) + 1).toString())}>increase detail level {detail}</Button></p>
+    </Paper>
+
     <pre>{[{location}, {query}].map(it => JSON.stringify(it)).join("\n")}</pre>
     <Box display="flex" flexWrap={"wrap"}>
     {
@@ -31,6 +43,7 @@ const ExploreData = () => {
     <pre>
       {JSON.stringify(outcomes, null, 2)}
     </pre>
+    <h1 id="anchor">anchor test</h1>
   </>
 }
 
