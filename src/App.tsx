8d923d66dcc208809b38b75b20902f5b429bab79
wormi <andreas.worm@headissue.com>
2020-09-25 12:52:40 +0200

    test router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import ExploreData from "./ExploreData";
+import {BrowserRouter as Router, Route, Link, useParams} from "react-router-dom";
 
 const App = () => {
-    return <div className="App">
-      <ExploreData/>
+
+  return <div className="App">
+      <Router>
+        <Route path="*" children={<ExploreData/>} />
+      </Router>
     </div>
   }
 ;
--- src/ExploreData.tsx
+++ src/ExploreData.tsx
@@ -2,37 +2,26 @@ import React, {useEffect, useState} from 'react';
 import TestOutcome from "./model/TestOutcome";
 import Outcome from "./Outcome";
 import {Box} from "@material-ui/core";
+import {useLocation, useParams} from "react-router";
+import qs from "query-string"
 //import Index from "../public/outcomes/full-json/index.json"
 
 declare global {
   interface Window {
-    outcomes:JSON;
+    outcomes:Array<TestOutcome>;
   }
 }
 
 
 const ExploreData = () => {
 
-  const [outcomes, setOutcomes] = useState<Array<TestOutcome>>([]);
+  const [outcomes, setOutcomes] = useState<Array<TestOutcome>>(window.outcomes);
 
-  console.log(window.outcomes)
-/*
-  useEffect(() => {
-    fetch('outcomes/full-json/index.json')
-      .then(data => data.json())
-      .then(files => {
-        Promise.all(files.map((it: RequestInfo) => fetch('outcomes/full-json/' + it)))
-          .then((results: Array<any>) => Promise.all(results.map((it) => it.json())))
-          .then((results) => setOutcomes(results))
-      })
-
-  }, [setOutcomes]);
-*/
-  if (outcomes.length === 0) {
-    return <></>;
-  }
+  const location = useLocation();
+  const query = qs.parse(location.search)
 
   return <>
+    <pre>{[{location}, {query}].map(it => JSON.stringify(it)).join("\n")}</pre>
     <Box display="flex" flexWrap={"wrap"}>
     {
       outcomes.map((it, i) => <Box key={i}><Outcome from={it} /></Box>)
