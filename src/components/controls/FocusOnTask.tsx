3da7fe6755ded7becc3d678d3089495763dd1cdf
wormi <andreas.worm@headissue.com>
2020-10-06 10:25:12 +0200

    more todo

--- src/components/controls/FocusOnTask.tsx
+++ src/components/controls/FocusOnTask.tsx
@@ -5,6 +5,7 @@ const FocusOnTask = () => {
   return <>
     <strong>view configurations for different use cases</strong><br/>
     <Button variant="contained" disableElevation href="?filter_testResult_exclude=SUCCESS&filter_testResult_exclude=PENDING&filter_testResult_exclude=IGNORED&view_detail=3&view_showScreenshots=true">debug mode</Button>
+    <Button disabled>TODO stories</Button><Button disabled>for specific issue</Button>
   </>
 };
 
