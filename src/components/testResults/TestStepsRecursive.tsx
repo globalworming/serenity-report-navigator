195459fdccad9fde31eb3dad684a5a2a92ffbef5
wormi <andreas.worm@headissue.com>
2020-10-08 00:27:42 +0200

    fix padding

--- src/components/testResults/TestStepsRecursive.tsx
+++ src/components/testResults/TestStepsRecursive.tsx
@@ -3,6 +3,7 @@ import TestStep from "../../model/TestStep";
 import Expandable from "../organisms/Expandable";
 import ResultImage from "../atoms/ResultImage";
 import FullWidthWrappingFlexBox from "../molecules/FullWidthWrappingFlexBox";
+import {Box} from "@material-ui/core";
 
 type MyProps = {
   tellAll?: Array<TestStep>,
@@ -12,9 +13,9 @@ type MyProps = {
 const TestStepsRecursive = ({tellAll, depth}: MyProps) => {
   if (!tellAll || tellAll.length === 0) return null;
 
-  const resultAndDescription = (testStep: TestStep) => <>
+  const resultAndDescription = (testStep: TestStep) => <Box style={{paddingLeft: `${0.1 + depth * 2}rem`}}>
     <ResultImage result={testStep.result}/> {testStep.description}
-  </>;
+  </Box>;
 
   const testStep = (step: TestStep) => <FullWidthWrappingFlexBox>{resultAndDescription(step)}</FullWidthWrappingFlexBox>;
   const expandableTestStep = (step: TestStep) => <Expandable expandOnGlobalDetail={3} whatsHidden={
