447432dcd74fd389a19791ae04ac4da9f950dab3
wormi <andreas.worm@headissue.com>
2020-10-13 22:13:15 +0200

    actual screenshots

--- src/components/testResults/screenshots/Screenshots.tsx
+++ src/components/testResults/screenshots/Screenshots.tsx
@@ -4,6 +4,10 @@ import FullWidthWrappingFlexBox from "../../molecules/FullWidthWrappingFlexBox";
 import TestOutcome from "../../../model/TestOutcome";
 import ResultImage from "../../atoms/ResultImage";
 import {colorOf} from "../../../model/Result";
+import {Button} from "@material-ui/core";
+import Fullscreen from "./noun_Expand proportionally_1691340.svg";
+import * as _ from "lodash";
+import Emoji from "../../atoms/Emoji";
 
 const flatSteps = (testSteps: Array<TestStep>) => {
   let result: Array<TestStep> = [];
@@ -18,36 +22,52 @@ interface MyProps {
   height: number
 }
 
+function hasScreenshots(it: TestStep) {
+  return it.screenshots && it.screenshots.length > 0;
+}
+
 const Screenshots = ({tell, height}: MyProps) => {
   const testSteps = flatSteps(tell.testSteps);
+  const screenshotToTestStep =
+  _.sortBy(testSteps
+    .filter(hasScreenshots)
+    .map(
+      step => step.screenshots.map(screenshot => ({step, screenshot: screenshot.screenshot}))
+    )
+    .flat(), it => it.step.number);
+  console.log({screenshotToTestStep});
 
-  const firstScreenshotIndex = testSteps.findIndex(it => it.screenshots && it.screenshots.length > 0);
-  const [index] = useState(firstScreenshotIndex);
+  const [index, setIndex] = useState(0);
 
-  const testStep = testSteps[index];
+  const {screenshot, step} = screenshotToTestStep[index];
+
 
   const style = {padding: "0.5rem", background: "#FFFFFFDD", color: "black"};
+  const controlStyle = {border: "3px solid #DDBBAADD", background: "#FFFFFFDD", margin: "0.5rem"} ;
   return <React.Fragment>
     {
-      testStep.screenshots.map(it => it.screenshot)
-        .map((it, j) =>
-          <FullWidthWrappingFlexBox key={`${j} ${it}`}
-                                    style={{
-                                      background: `url("./screenshots/${it}")`,
-                                      backgroundSize: "cover",
-                                      //backgroundPosition: "center",
-                                      backgroundRepeat: "no-repeat",
-                                      boxShadow: `${colorOf(tell.result)} -1px -1px 10px 2px`
-                                    }}
+          <FullWidthWrappingFlexBox
+            style={{
+              backgroundImage: `url("./screenshots/${screenshot}")`,
+              backgroundSize: "cover",
+              //backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              boxShadow: `${colorOf(tell.result)} -1px -1px 10px 2px`
+            }}
           >
             <span style={style}><ResultImage result={tell.result}/> {tell.title}</span>
             <FullWidthWrappingFlexBox style={{paddingTop: height + "rem"}}>
-
-              <span style={style}>{testStep.description}</span>
+              <span style={style}><ResultImage result={step.result}/> {step.description}</span>
             </FullWidthWrappingFlexBox>
-            <span>controls</span>
+            <FullWidthWrappingFlexBox style={{paddingBottom: "1rem"}}>
+              <Button style={controlStyle} variant={"text"} onClick={() => setIndex(Math.max(index - 1, 0))}>&lt;</Button>
+              {
+                screenshotToTestStep.map((_, i) => <Button key={i} style={{minWidth: "20px"}} variant={"text"}>{i === index ? <Emoji label={"screenshots"} /> : "*"}</Button>)
+              }
+              <Button style={controlStyle} variant={"text"} onClick={() => setIndex(Math.min(index + 1, screenshotToTestStep.length - 1))}>&gt;</Button>
+              <Button style={controlStyle} variant={"text"} onClick={() => alert("coming soon")}><img alt={"fullscreen"} style={{width: "2rem"}} src={Fullscreen}/></Button>
+            </FullWidthWrappingFlexBox>
           </FullWidthWrappingFlexBox>
-        )
     }
   </React.Fragment>
 };
