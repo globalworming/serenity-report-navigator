50e40ddcbf7cdd53faea34140e20dc5df00dd648
wormi <andreas.worm@headissue.com>
2020-11-01 23:14:51 +0100

    links can contain custom theme

--- src/components/testResults/outcome/LinkTo.tsx
+++ src/components/testResults/outcome/LinkTo.tsx
@@ -1,11 +1,15 @@
 import {MyQuery, queryString} from "../../LocalStateFromQueryParameters";
-import {Button} from "@material-ui/core";
+import {Button, useTheme} from "@material-ui/core";
 import React from "react";
 import useGlobalState from "../../../state";
 
 const LinkTo = (props: MyQuery) => {
-  const [theme] = useGlobalState("theme");
-  return <Button  variant={"text"} onClick={e => e.stopPropagation()} target={"_blank"} href={queryString(Object.assign({theme}, props))}>
+  const [themeKey] = useGlobalState("theme");
+  const theme = useTheme();
+
+  const encodedTheme = themeKey !== "custom" ? themeKey :
+    [theme.palette.primary.main, theme.palette.secondary.main, theme.palette.text.primary, theme.palette.background.default, theme.palette.background.paper].join("_");
+  return <Button  variant={"text"} onClick={e => e.stopPropagation()} target={"_blank"} href={queryString(Object.assign({theme: encodedTheme}, props))}>
     <span role="img" aria-label={"link"}>🔗</span></Button>
 };
 
