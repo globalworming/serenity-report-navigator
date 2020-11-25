2246b4d1dcf140fa5bb3eae7c218e3d76330e17e
globalworming <aw_github@headissue.com>
2020-11-25 14:46:06 +0100

    limit to 5 outcomes at a time

--- src/components/explore/ByScreenshots.tsx
+++ src/components/explore/ByScreenshots.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import _ from 'lodash';
 import useGlobalState from '../../state';
 import FullWidthWrappingFlexBox from "../molecules/FullWidthWrappingFlexBox";
 import TestStep, {Screenshot} from "../../model/TestStep";
 import Emoji from "../atoms/Emoji";
 import Screenshots from "./screenshots/Screenshots";
+import {Button} from "@material-ui/core";
 
 const findScreenshots = (testSteps: Array<TestStep>) => {
   let steps: Array<Screenshot> = [];
@@ -17,8 +18,10 @@ const findScreenshots = (testSteps: Array<TestStep>) => {
 
 const ByScreenshots = () => {
   let [outcomes] = useGlobalState('filteredOutcomes');
-  const localOutcomes = outcomes.filter(it => findScreenshots(it.testSteps).length > 0);
-  const outcomesByStoryId = _.groupBy(localOutcomes, o => o.userStory.id);
+  let [itemsDisplayed, setItemsDisplayed] = useState(5);
+  const outcomesWithScreenshots = outcomes.filter(it => findScreenshots(it.testSteps).length > 0);
+  const sorted = _.sortBy(outcomesWithScreenshots, it => it.userStory.id).slice(0, itemsDisplayed);
+  const outcomesByStoryId = _.groupBy(sorted, o => o.userStory.id);
 
   const width = 30;
   return <>
@@ -26,12 +29,13 @@ const ByScreenshots = () => {
       {_.keys(outcomesByStoryId).map((storyId) => {
         return <React.Fragment key={storyId}>
           <FullWidthWrappingFlexBox style={{paddingTop: "1rem"}}>
-            <FullWidthWrappingFlexBox style={{justifyContent: "center", fontSize: "2rem"}}><Emoji label={"story"}/>&nbsp;{outcomesByStoryId[storyId][0].userStory.storyName}</FullWidthWrappingFlexBox>
+            <FullWidthWrappingFlexBox style={{justifyContent: "center", fontSize: "2rem"}}><Emoji
+              label={"story"}/>&nbsp;{outcomesByStoryId[storyId][0].userStory.storyName}</FullWidthWrappingFlexBox>
             {
               outcomesByStoryId[storyId].map((outcome, i) =>
                 <React.Fragment key={i + storyId}>
-                  <FullWidthWrappingFlexBox style={{flex: `1 1 ${width* 2}rem`}}>
-                    <Screenshots tell={outcome} width={width} />
+                  <FullWidthWrappingFlexBox style={{flex: `1 1 ${width * 2}rem`}}>
+                    <Screenshots tell={outcome} width={width}/>
                   </FullWidthWrappingFlexBox>
                 </React.Fragment>
               )
@@ -40,6 +44,13 @@ const ByScreenshots = () => {
         </React.Fragment>;
       })}
     </FullWidthWrappingFlexBox>
+    {itemsDisplayed < outcomesWithScreenshots.length && <FullWidthWrappingFlexBox>
+      <Button variant={"contained"} size={"small"} color={"secondary"} fullWidth style={{margin: "1rem"}}
+              onClick={() => setItemsDisplayed(itemsDisplayed + 5)}>
+        show more..
+      </Button>
+    </FullWidthWrappingFlexBox>
+    }
   </>
 };
 
